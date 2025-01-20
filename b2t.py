481e4f60f9c3fbcde53a40f725453e7c4af22dc7
Xinyu Zhu <xyz.hm@hotmail.com>
2025-01-20 15:19:12 +0100

    imagenet-r

--- b2t.py
+++ b2t.py
@@ -16,6 +16,8 @@ from function.extract_keyword import extract_keyword
 from function.calculate_similarity import calc_similarity
 from function.print_similarity import print_similarity
 
+from mapping_labels import map_folder_to_imagenet
+
 from tqdm import tqdm
 import os
 import time
@@ -64,12 +66,19 @@ elif args.dataset == 'imagenet-r':
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-    class_names = [str(i) for i in range(200)]
+    reverse_mapping = {v: k for k, v in map_folder_to_imagenet.items()}
+    # print(reverse_mapping)
     # tested on subset of data, to test on real data: use 'data/imagenetR/data/imagenet-r/'
     image_dir = ''
     caption_dir = 'data/imagenetR/caption/'
-    val_dataset = dataset.ImageFolder(root="data/imagenetR/data/imagenet-r-test", transform=preprocess)
-
+    val_dataset = dataset.ImageFolder(root="data/imagenetR/data/imagenet-r-test", transform=preprocess, 
+                                      target_transform=lambda label: reverse_mapping[val_dataset.classes[label]])
+    class_names = [reverse_mapping[label] for label in val_dataset.classes]
+    print(class_names)
+    # print(val_dataset.classes)
+    # print(all_folder_names)
+    # print(val_dataset[0])
+    # print(val_dataset[160])
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=256, num_workers=4, drop_last=False)
 
 
@@ -114,20 +123,14 @@ if args.extract_caption:
 result_path = result_dir + args.dataset +"_" +  args.model.split(".")[0] + ".csv"
 if not os.path.exists(result_path):
     if args.dataset == 'imagenet-r':
-        model = models.resnet50(pretrained=False)
-        checkpoint = torch.load(model_dir + args.model, map_location=torch.device('cpu'))
-        model.load_state_dict(checkpoint)
-        model = model.to(device)
-        model.eval()
-        start_time = time.time()
-        print("Pretrained model \"{}\" loaded".format(args.model))
+        model = models.resnet50(weights="IMAGENET1K_V1")
     else: 
-        model = torch.load(model_dir + args.model, map_location=torch.device('cpu'))
-        model = model.to(device)
-        model.eval()
-        start_time = time.time()
-        print("Pretrained model \"{}\" loaded".format(args.model))
+        model = torch.load(model_dir + args.model, map_location=device)
 
+    model = model.to(device)
+    model.eval()
+    start_time = time.time()
+    print("Pretrained model \"{}\" loaded".format(args.model))
     if args.dataset == 'imagenet-r':
         result = {"image":[],
                 "pred":[],
@@ -204,12 +207,13 @@ if not os.path.exists(result_path):
         print("# of wrong examples : ", len(val_dataset) - running_corrects)
         print("# of all examples : ", len(val_dataset))
         print("Accuracy : {:.2f} %".format(running_corrects/len(val_dataset)*100))
-    print(result)
-    print('image', len(result['image']))
-    print('pred',len(result['pred']))
-    print('actual',len(result['actual']))
-    print('caption',len(result['caption']))
-    print('correct',len(result['correct']))
+
+    # print(result)
+    # print('image', len(result['image']))
+    # print('pred',len(result['pred']))
+    # print('actual',len(result['actual']))
+    # print('caption',len(result['caption']))
+    # print('correct',len(result['correct']))
     
     df = pd.DataFrame(result)
     df.to_csv(result_path)
@@ -219,39 +223,78 @@ else:
     print("Classified result \"{}\" loaded".format(result_path))
 
 # extract keyword
-df_wrong = df[df['correct'] == 0]
-df_correct = df[df['correct'] == 1]
-df_class_0 = df[df['actual'] == 0] # not blond, landbird
-df_class_1 = df[df['actual'] == 1] # blond, waterbird
-df_wrong_class_0 = df_wrong[df_wrong['actual'] == 0]
-df_wrong_class_1 = df_wrong[df_wrong['actual'] == 1]
-df_correct_class_0 = df_correct[df_correct['actual'] == 0]
-df_correct_class_1 = df_correct[df_correct['actual'] == 1]
+if args.dataset == 'imagenet-r':
+    df_wrong = df[df['correct'] == 0]
+    df_correct = df[df['correct'] == 1]
+    for labels in class_names: 
+        df_class = df[df['actual'] == labels] 
+        df_wrong_class = df_wrong[df_wrong['actual'] == labels]
+        df_correct_class = df_correct[df_correct['actual'] == labels]
+        caption_wrong_class = ' '.join(df_wrong_class['caption'].tolist())
+        keywords_class = extract_keyword(caption_wrong_class)
 
-caption_wrong_class_0 = ' '.join(df_wrong_class_0['caption'].tolist())
-caption_wrong_class_1 = ' '.join(df_wrong_class_1['caption'].tolist())
+        # calculate similarity
+        print("Start calculating scores..")
+        similarity_wrong_class = calc_similarity(image_dir, df_wrong_class['image'], keywords_class)
+        similarity_correct_class = calc_similarity(image_dir, df_correct_class['image'], keywords_class)
 
-keywords_class_0 = extract_keyword(caption_wrong_class_0)
-keywords_class_1 = extract_keyword(caption_wrong_class_1)
+        dist_class = similarity_wrong_class - similarity_correct_class
+        
+        print("Result for class :", labels)
+        diff_0 = print_similarity(keywords_class, keywords_class, dist_class, dist_class, df_class)
+        print("*"*60)
 
-# calculate similarity
-print("Start calculating scores..")
-similarity_wrong_class_0 = calc_similarity(image_dir, df_wrong_class_0['image'], keywords_class_0)
-similarity_correct_class_0 = calc_similarity(image_dir, df_correct_class_0['image'], keywords_class_0)
-similarity_wrong_class_1 = calc_similarity(image_dir, df_wrong_class_1['image'], keywords_class_1)
-similarity_correct_class_1 = calc_similarity(image_dir, df_correct_class_1['image'], keywords_class_1)
+        if args.save_result:
+            diff_path = diff_dir + args.dataset +"_" +  args.model.split(".")[0] + "_" +  str(labels) + ".csv"
+            diff_0.to_csv(diff_path)
 
-dist_class_0 = similarity_wrong_class_0 - similarity_correct_class_0
-dist_class_1 = similarity_wrong_class_1 - similarity_correct_class_1
+else:
+    df_wrong = df[df['correct'] == 0]
+    df_correct = df[df['correct'] == 1]
+    df_class_0 = df[df['actual'] == 0] # not blond, landbird
+    df_class_1 = df[df['actual'] == 1] # blond, waterbird
+    df_wrong_class_0 = df_wrong[df_wrong['actual'] == 0]
+    df_wrong_class_1 = df_wrong[df_wrong['actual'] == 1]
+    df_correct_class_0 = df_correct[df_correct['actual'] == 0]
+    df_correct_class_1 = df_correct[df_correct['actual'] == 1]
 
-print("Result for class :", class_names[0])
-diff_0 = print_similarity(keywords_class_0, keywords_class_1, dist_class_0, dist_class_1, df_class_0)
-print("*"*60)
-print("Result for class :", class_names[1])
-diff_1 = print_similarity(keywords_class_1, keywords_class_0, dist_class_1, dist_class_0, df_class_1)
+# print(df_wrong)
+# print(df_correct)
+# print(df_class_0)
+# print(df_class_1)
+# print(df_wrong_class_0)
+# print(df_wrong_class_1)
+# print(df_correct_class_0)
+# print(df_correct_class_1)
 
-if args.save_result:
-    diff_path_0 = diff_dir + args.dataset +"_" +  args.model.split(".")[0] + "_" +  class_names[0] + ".csv"
-    diff_path_1 = diff_dir + args.dataset +"_" +  args.model.split(".")[0] + "_" +  class_names[1] + ".csv"
-    diff_0.to_csv(diff_path_0)
-    diff_1.to_csv(diff_path_1)
+    caption_wrong_class_0 = ' '.join(df_wrong_class_0['caption'].tolist())
+    caption_wrong_class_1 = ' '.join(df_wrong_class_1['caption'].tolist())
+    # print(caption_wrong_class_0)
+    # print(caption_wrong_class_1)
+
+    keywords_class_0 = extract_keyword(caption_wrong_class_0)
+    keywords_class_1 = extract_keyword(caption_wrong_class_1)
+    # print(keywords_class_0)
+    # print(keywords_class_1)
+
+    # calculate similarity
+    print("Start calculating scores..")
+    similarity_wrong_class_0 = calc_similarity(image_dir, df_wrong_class_0['image'], keywords_class_0)
+    similarity_correct_class_0 = calc_similarity(image_dir, df_correct_class_0['image'], keywords_class_0)
+    similarity_wrong_class_1 = calc_similarity(image_dir, df_wrong_class_1['image'], keywords_class_1)
+    similarity_correct_class_1 = calc_similarity(image_dir, df_correct_class_1['image'], keywords_class_1)
+
+    dist_class_0 = similarity_wrong_class_0 - similarity_correct_class_0
+    dist_class_1 = similarity_wrong_class_1 - similarity_correct_class_1
+
+    print("Result for class :", class_names[0])
+    diff_0 = print_similarity(keywords_class_0, keywords_class_1, dist_class_0, dist_class_1, df_class_0)
+    print("*"*60)
+    print("Result for class :", class_names[1])
+    diff_1 = print_similarity(keywords_class_1, keywords_class_0, dist_class_1, dist_class_0, df_class_1)
+
+    if args.save_result:
+        diff_path_0 = diff_dir + args.dataset +"_" +  args.model.split(".")[0] + "_" +  class_names[0] + ".csv"
+        diff_path_1 = diff_dir + args.dataset +"_" +  args.model.split(".")[0] + "_" +  class_names[1] + ".csv"
+        diff_0.to_csv(diff_path_0)
+        diff_1.to_csv(diff_path_1)
