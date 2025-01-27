41cb66aa7bface6a66284d9bcbd614917d6ae2d8
koenveldhorst <koen.veldhorst@gmail.com>
2025-01-27 21:15:13 +0100

    plots figure 3b + 3c

--- plot-roc-curve.py
+++ plot-roc-curve.py
@@ -1,3 +1,4 @@
+from scipy.stats import linregress
 from sklearn.metrics import roc_curve, auc
 from data.waterbirds import get_transform_cub
 from PIL import Image
@@ -134,13 +135,15 @@ def compute_roc_and_auc(predicted_scores, true_labels):
     return fpr, tpr, roc_auc
 
 
-def plot_roc_curves(subgroup_data, keywords, clip_scores, csv_file_path):
+def plot_roc_curves(subgroup_data, keywords, clip_scores, fig_path):
     """
     Generate and plot ROC curves for each subgroup (keyword).
 
     Parameters:
         subgroup_data (dict): Dictionary where keys are keywords and values are tuples of (predicted_scores, true_labels).
         keywords (list): List of keywords to plot ROC curves for.
+        clip_scores (dict): Dictionary where keys are keywords and values are CLIP scores.
+        fig_path (str): Path to save the figure.
     """
     plt.figure(figsize=(6, 5))
 
@@ -164,8 +167,69 @@ def plot_roc_curves(subgroup_data, keywords, clip_scores, csv_file_path):
     plt.xlim([0, 1])
     plt.ylim([0, 1])
 
+    fig_path = fig_path + "_roc_plot.pdf"
+    plt.savefig(f"{fig_path}", dpi=300, bbox_inches="tight")
+    print(f"Saved figure to {fig_path}")
 
-    fig_path = "plots/" + csv_file_path.split(".")[0].split("/")[1].split("_")[0] + "_roc_plot.png"
+
+def plot_correlation(subgroup_data, keywords, clip_scores, fig_path):
+    """
+    Generate and plot correlation between AUC and CLIP scores.
+
+    Parameters:
+        subgroup_data (dict): Dictionary where keys are keywords and values are tuples of (predicted_scores, true_labels).
+        keywords (list): List of keywords to plot correlation for.
+        clip_scores (dict): Dictionary where keys are keywords and values are CLIP scores.
+        fig_path (str): Path to save the figure.
+    """
+    plt.figure(figsize=(6, 5))
+
+    auc_scores = []
+    for keyword in keywords:
+        if keyword not in subgroup_data:
+            print(f"Keyword '{keyword}' not found in data. Skipping.")
+            continue
+
+        predicted_scores, true_labels = subgroup_data[keyword]
+        _, _, roc_auc = compute_roc_and_auc(predicted_scores, true_labels)
+        auc_scores.append((clip_scores[keyword], roc_auc))
+
+    auc_scores = np.array(auc_scores)
+    clip_scores_array = auc_scores[:, 0]
+    auc_values = auc_scores[:, 1]
+
+    slope, intercept, _, _, _ = linregress(clip_scores_array, auc_values)
+    x_range = np.linspace(np.min(clip_scores_array), np.max(clip_scores_array), 100)
+    y_range = slope * x_range + intercept
+
+    y_pred = slope * clip_scores_array + intercept
+    residuals = auc_values - y_pred
+    mse = np.mean(residuals ** 2)
+    ci = 1.96 * np.sqrt(
+        mse * (1 / len(clip_scores_array)
+        + (x_range - np.mean(clip_scores_array))**2
+        / np.sum((clip_scores_array - np.mean(clip_scores_array))**2))
+    )
+
+    plt.scatter(clip_scores_array, auc_values, s=50, color="teal", alpha=0.8)
+    plt.plot(x_range, y_range, color="gray", linestyle="--", linewidth=2)
+    plt.xlabel("CLIP Score", fontsize=14)
+    plt.ylabel("AUC Score", fontsize=14)
+    plt.title("Correlation between AUC and CLIP Score", fontsize=14)
+
+    plt.fill_between(x_range, y_range - ci, y_range + ci, color="gray", alpha=0.2)
+
+    plt.text(
+        x=np.max(clip_scores_array) - 1.15,
+        y=np.max(auc_values) - 0.01,
+        s=f"coef = {slope:.3f}",
+        fontsize=12,
+        color="black",
+    )
+
+    plt.tight_layout()
+
+    fig_path = fig_path + "_AUC_clip_corr_plot.pdf"
     plt.savefig(f"{fig_path}", dpi=300, bbox_inches="tight")
     print(f"Saved figure to {fig_path}")
 
@@ -174,13 +238,15 @@ if __name__ == "__main__":
     image_dir = "data/cub/data/waterbird_complete95_forest2water2/"
     results_csv = "result/waterbird_best_model_Waterbirds_erm.csv"
     b2t_csv = "diff/waterbird_best_model_Waterbirds_erm_waterbird.csv"
-    keywords = ["bamboo", "forest", "woods", "species", "bird"]
+    fig_path = "plots/" + b2t_csv.split(".")[0].split("/")[1].split("_")[-1]
 
     # Load in the csv files
     results = pd.read_csv(results_csv)
     b2t_scores = pd.read_csv(b2t_csv)
 
     # Clip scores per keyword + image subset
+    keywords = b2t_scores.groupby("Keyword")["Score"].mean()
+    keywords = keywords.sort_values(ascending=False).index.tolist()
     clip_scores = b2t_scores.loc[b2t_scores["Keyword"].isin(keywords), "Score"].values
     clip_scores = {keyword: score for keyword, score in zip(keywords, clip_scores)}
     images = results.loc[results["actual"] == 1, "image"].to_list() # Only waterbird images
@@ -193,4 +259,6 @@ if __name__ == "__main__":
     # Prepare data for plotting
     subgroup_data = {keyword: (classifier_scores, subgroup_labels[keyword][1]) for keyword in keywords}
 
-    plot_roc_curves(subgroup_data, keywords, clip_scores, results_csv)
+    roc_keywords = ["bamboo", "forest", "woods", "species", "bird"]
+    plot_roc_curves(subgroup_data, roc_keywords, clip_scores, fig_path)
+    # plot_correlation(subgroup_data, keywords, clip_scores, fig_path)
