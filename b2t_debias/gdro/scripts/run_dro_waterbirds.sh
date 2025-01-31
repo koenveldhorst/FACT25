f7734fb0f1eb32c6e5ab7af1d98b30f74432e3c7
Mor ris <morrisdh18@gmail.com>
2025-01-31 05:38:45 +0100

    fix in dro script

--- b2t_debias/gdro/scripts/run_dro_waterbirds.sh
+++ b2t_debias/gdro/scripts/run_dro_waterbirds.sh
@@ -3,6 +3,6 @@ SEED=$2
 
 python group_dro.py --name gdro_resnet50_lr_1e-5_wd_1_epoch_300_seed_$SEED \
 --dataset cub --image_size 224 \
---data_root DATA_ROOT \
+--data_root $DATA_ROOT \
 --model resnet50 --pretrained imagenet --num_classes 2 \
 --epochs 300 --lr 1e-5 --weight-decay 1 --seed $SEED
